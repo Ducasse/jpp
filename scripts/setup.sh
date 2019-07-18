8127ad5a163d614cb6ad44330f4e0e72778414bf
Julien Delplanque <julien.delplanque@live.be>
2019-07-18 18:20:06 +0200

    Added setup script.

---
+++ scripts/setup.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# URLs
+IMAGE_URL="https://get.pharo.org/70-minimal"
+VM_URL="https://get.pharo.org/vm70"
+SOURCES_URL="http://files.pharo.org/sources/PharoV60.sources"
+TONEL_URL="github://pharo-vcs/tonel"
+JPP_URL="github://juliendelplanque/jpp/src"
+# Baselines
+TONEL_BASELINE="BaselineOfTonel"
+JPP_BASELINE="BaselineOfJSONPreprocessor"
+# Commands
+METACELLO_CMD="./pharo Pharo.image metacello"
+
+################################################################################
+
+# Create directory and enter it.
+mkdir "jpp"
+cd "jpp"
+
+# Download image, vm and sources files.
+curl "$IMAGE_URL" | bash
+curl "$VM_URL" | bash
+wget "$SOURCES_URL"
+
+# Install Tonel (required because jpp is in Tonel format).
+eval "$METACELLO_CMD install $TONEL_URL $TONEL_BASELINE"
+# Install jpp.
+eval "$METACELLO_CMD install $JPP_URL $JPP_BASELINE"
+
+# Clean garbage.
+rm -rf github-cache
+rm -rf pharo-local
+rm -rf image.*
+rm -f pharo-ui
+
+# Go back to original directory.
+cd ..
