9cb24bd7a2a2f1db833904b87b2b4c087775eef0
Julien Delplanque <julien.delplanque@live.be>
2020-01-03 11:21:08 +0100

    Added test.

--- shell-tests/jpp-tests.sh
+++ shell-tests/jpp-tests.sh
@@ -6,4 +6,27 @@ testBonjour() {
 	assertEquals $(echo '{"_format" : "bbcode", "_string" : "[b]Bonjour[/b]"}' | $JPP) '"<b>Bonjour</b>"'
 }
 
+testListMetadata() {
+	local input='[{"name" : "Julien", "age" : 25}, {"name" : "Alexandre", "age" : 22}]'
+	read -r -d '' output << EOM
+[
+	{
+		"_index" : 1,
+		"age" : 25,
+		"_last?" : false,
+		"_first?" : true,
+		"name" : "Julien"
+	},
+	{
+		"_index" : 2,
+		"age" : 22,
+		"_last?" : true,
+		"_first?" : false,
+		"name" : "Alexandre"
+	}
+]
+EOM
+	assertEquals "$(echo $input | $JPP)" "$output"
+}
+
 . shunit2-2.1.6/src/shunit2
